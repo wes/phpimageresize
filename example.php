1df2b27e78013856f3deebe4192882a1d8da8e02
Wes Edling <wes@wes-edlings-powerbook-g4-17.local>
2009-07-01 01:11:27 -0600

    update to example page

--- example.php
+++ example.php
@@ -1,6 +1,7 @@
 <?php
 # include the function here
 include 'function.resize.php';
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -10,28 +11,47 @@ include 'function.resize.php';
 	<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 	<title>PHP Image Resize - Example</title>
 	<style>
-		body { background: #121212; color: #ffffff; font-family: lucida grande; text-align: center; }
+		body { 
+			background: #121212; 
+			color: #ffffff; 
+			font-family: lucida grande; 
+			text-align: center; 
+		}
+		#main { margin: auto; width: 400px; text-align: left; }
 		.block { margin: 20px; }
+		pre { text-align: left; width: 400px; background: #010101; padding: 10px; }
 	</style>
 </head>
 
 <body>
 
+<div id='main'>
+
 	<div class='block'>
-		<img src='<?=resize('images/dog.jpg',array('w'=>300))?>' border='0' />
+		<?php $settings = array('w'=>300); ?>
+		<div><img src='<?=resize('images/dog.jpg',$settings)?>' border='0' /></div>
+		<div><pre><code><?php print_r($settings)?></code></pre></div>
 	</div>
 
 	<div class='block'>
-		<img src='<?=resize('images/dog.jpg',array('w'=>300,'h'=>300))?>' border='0' />
+		<?php $settings = array('w'=>300,'h'=>300); ?>
+		<div><img src='<?=resize('images/dog.jpg',$settings)?>' border='0' /></div>
+		<div><pre><code><?php print_r($settings)?></code></pre></div>
 	</div>
 
 	<div class='block'>
-		<img src='<?=resize('images/dog.jpg',array('w'=>200,'h'=>200,'crop'=>true))?>' border='0' />
+		<?php $settings = array('w'=>300,'h'=>300,'crop'=>true); ?>
+		<div><img src='<?=resize('images/dog.jpg',$settings)?>' border='0' /></div>
+		<div><pre><code><?php print_r($settings)?></code></pre></div>
 	</div>
 
 	<div class='block'>
-		<img src='<?=resize('images/dog.jpg',array('w'=>300,'h'=>300,'scale'=>true))?>' border='0' />
+		<?php $settings = array('w'=>300,'h'=>300,'scale'=>true); ?>
+		<div><img src='<?=resize('images/dog.jpg',$settings)?>' border='0' /></div>
+		<div><pre><code><?php print_r($settings)?></code></pre></div>
 	</div>
+
+</div>
 
 </body>
 </html>
