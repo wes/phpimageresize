593e4d58324f4ffb9bb26c85641784a647e966d1
Wes Edling <wes@wes-edlings-powerbook-g4-17.local>
2009-07-01 01:29:29 -0600

    update to styles

--- example.php
+++ example.php
@@ -12,15 +12,17 @@ include 'function.resize.php';
 	<title>PHP Image Resize - Example</title>
 	<style>
 		body { 
-			background: #121212; 
-			color: #ffffff; 
+			background: #ffffff; 
+			color: #121212; 
 			font-family: lucida grande; 
 			text-align: center; 
 		}
 		h1 { font-size: 15px; text-align: center; }
-		#main { margin: auto; width: 400px; text-align: left; }
-		.block { margin: 20px; background: #cacaca; padding: 20px; text-align: center; }
-		pre { text-align: left; width: 300px; background: #010101; padding: 10px; font-size: 11px; }
+		#main { margin: auto; width: 600px; text-align: left; }
+		.block { margin: 20px; background: #fafafa; padding: 20px; text-align: center; border: 1px solid #cacaca; }
+		pre { text-align: left; background: #010101; padding: 10px; font-size: 11px; }
+		pre code { text-align: left; color: #ffffff; }
+		.block p { color: #343434; font-size: 12px; }
 	</style>
 </head>
 
@@ -33,31 +35,36 @@ include 'function.resize.php';
 	<div class='block'>
 		<?php $settings = array('w'=>300); ?>
 		<div><img src='<?=resize('images/dog.jpg',$settings)?>' border='0' /></div>
-		<div><pre><code><?php print_r($settings)?></code></pre></div>
+		<p>Image resized by width only</p>
+		<div><pre><code>src: images/dog.jpg<?php echo "\n\n"; print_r($settings)?></code></pre></div>
 	</div>
 
 	<div class='block'>
 		<?php $settings = array('w'=>300,'h'=>300); ?>
 		<div><img src='<?=resize('images/dog.jpg',$settings)?>' border='0' /></div>
-		<div><pre><code><?php print_r($settings)?></code></pre></div>
+		<p>Image resized by width and height</p>
+		<div><pre><code>src: images/dog.jpg<?php echo "\n\n"; print_r($settings)?></code></pre></div>
 	</div>
 
 	<div class='block'>
 		<?php $settings = array('w'=>300,'h'=>300,'crop'=>true); ?>
 		<div><img src='<?=resize('images/dog.jpg',$settings)?>' border='0' /></div>
-		<div><pre><code><?php print_r($settings)?></code></pre></div>
+		<p>Image cropped &amp; resized by width and height</p>
+		<div><pre><code>src: images/dog.jpg<?php echo "\n\n"; print_r($settings)?></code></pre></div>
 	</div>
 
 	<div class='block'>
 		<?php $settings = array('w'=>300,'h'=>300,'scale'=>true); ?>
 		<div><img src='<?=resize('images/dog.jpg',$settings)?>' border='0' /></div>
-		<div><pre><code><?php print_r($settings)?></code></pre></div>
+		<p>Image scaled by width and height</p>
+		<div><pre><code>src: images/dog.jpg<?php echo "\n\n"; print_r($settings)?></code></pre></div>
 	</div>
 
 	<div class='block'>
 		<?php $settings = array('w'=>100,'h'=>100,'crop'=>true); ?>
 		<div><img src='<?=resize('http://farm4.static.flickr.com/3210/2934973285_fa4761c982.jpg',$settings)?>' border='0' /></div>
-		<div><pre><code><?php print_r($settings)?></code></pre></div>
+		<p>Image cropped &amp; resized by width and height from a remote location.</p>
+		<div><pre><code>src: http://farm4.static.flickr.com/3210/2934973285_fa4761c982.jpg<?php echo "\n\n"; print_r($settings)?></code></pre></div>
 	</div>
 
 </div>
