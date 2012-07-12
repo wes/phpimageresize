ce2c7049dc6503496564215c72e9cdba7b077505
Nishant Arora <wrongnumber.fu@gmail.com>
2012-07-12 20:26:46 +0530

    Some Code Cleaning
    I relaized that there were some lines that were either were not
    performing anything or were redundant. like the defaults were set at the
    start, so we need not check if the variables are set or not when we are
    resizing 100s of images, this can cause a lag of many milliseconds as it
    is just checking if it is set or not set.
    
    Also I changed the syntax to parenthesis oriented, makes it easier to
    understand and edit (folding of functions). also somewhere i removed a
    variable that was not needed.
    
    There was also an issue regarding 'scale' => 'false' i think it's okay
    to make it 'scale' => false
    
    Still Please test the working before pulling. here is the working
    example http://clients.nishantarora.in/projects/img_r_u/example.php

--- function.resize.php
+++ function.resize.php
@@ -4,12 +4,11 @@
  * feel free to use this in any project, i just ask for a credit in the source code.
  * a link back to my site would be nice too.
  *
- *
  * Changes: 
  * 2012/01/30 - David Goodwin - call escapeshellarg on parameters going into the shell
  * 2012/07/12 - Whizzkid - Added support for encoded image urls and images on ssl secured servers [https://]
+ * 2012/07/12 - Whizzkid - Code Cleaning...
  */
-
 /**
  * SECURITY:
  * It's a bad idea to allow user supplied data to become the path for the image you wish to retrieve, as this allows them
@@ -18,7 +17,6 @@
  * <code>php_flag engine off</code>
  * to at least stop arbitrary code execution. You can deal with any copyright infringement issues yourself :)
  */
-
 /**
  * @param string $imagePath - either a local absolute/relative path, or a remote URL (e.g. http://...flickr.com/.../ ). See SECURITY note above.
  * @param array $opts  (w(pixels), h(pixels), crop(boolean), scale(boolean), thumbnail(boolean), maxOnly(boolean), canvas-color(#abcabc), output-filename(string), cache_http_minutes(int))
@@ -26,126 +24,113 @@
  */
 function resize($imagePath,$opts=null){
 	$imagePath = urldecode($imagePath);
-	# start configuration
-	$cacheFolder = './cache/'; # path to your cache folder, must be writeable by web server
-	$remoteFolder = $cacheFolder.'remote/'; # path to the folder you wish to download remote images into
-
-	$defaults = array('crop' => false, 'scale' => 'false', 'thumbnail' => false, 'maxOnly' => false, 
-	   'canvas-color' => 'transparent', 'output-filename' => false, 
-	   'cacheFolder' => $cacheFolder, 'remoteFolder' => $remoteFolder, 'quality' => 90, 'cache_http_minutes' => 20);
-
-	$opts = array_merge($defaults, $opts);    
-
-	$cacheFolder = $opts['cacheFolder'];
-	$remoteFolder = $opts['remoteFolder'];
-
-	$path_to_convert = 'convert'; # this could be something like /usr/bin/convert or /opt/local/share/bin/convert
+	// start configuration........
+	$cacheFolder = './cache/';							//path to your cache folder, must be writeable by web server
+	$remoteFolder = $cacheFolder.'remote/';				//path to the folder you wish to download remote images into
+	$defaults = array(
+					'crop'					=> false,
+					'scale'					=> false,
+					'thumbnail'				=> false,
+					'maxOnly'				=> false,
+					'canvas-color'			=> 'transparent',
+					'output-filename'		=> false,
+					'cacheFolder'			=> $cacheFolder,
+					'remoteFolder'			=> $remoteFolder,
+					'quality'				=> 90,
+					'cache_http_minutes'	=> 0
+				);
+	$opts = array_merge($defaults, $opts);
+	$path_to_convert = 'convert';						//this could be something like /usr/bin/convert or /opt/local/share/bin/convert
+	// configuration ends...
 	
-	## you shouldn't need to configure anything else beyond this point
-
 	$purl = parse_url($imagePath);
 	$finfo = pathinfo($imagePath);
 	$ext = $finfo['extension'];
-
-	# check for remote image..
-	if(isset($purl['scheme']) && ($purl['scheme'] == 'http' || $purl['scheme'] == 'https')):
-		# grab the image, and cache it so we have something to work with..
+	// check for remote image..
+	if(isset($purl['scheme']) && ($purl['scheme'] == 'http' || $purl['scheme'] == 'https')){
+	// grab the image, and cache it so we have something to work with..
 		list($filename) = explode('?',$finfo['basename']);
 		$local_filepath = $remoteFolder.$filename;
 		$download_image = true;
-		if(file_exists($local_filepath)):
-			if(filemtime($local_filepath) < strtotime('+'.$opts['cache_http_minutes'].' minutes')):
+		if(file_exists($local_filepath)){
+			if(filemtime($local_filepath) < strtotime('+'.$opts['cache_http_minutes'].' minutes')){
 				$download_image = false;
-			endif;
-		endif;
-		if($download_image == true):
-			$img = file_get_contents($imagePath);
-			file_put_contents($local_filepath,$img);
-		endif;
+			}
+		}
+		if($download_image == true){
+			file_put_contents($local_filepath,file_get_contents($imagePath));
+		}
 		$imagePath = $local_filepath;
-	endif;
-
-	if(file_exists($imagePath) == false):
+	}
+	if(file_exists($imagePath) == false){
 		$imagePath = $_SERVER['DOCUMENT_ROOT'].$imagePath;
-		if(file_exists($imagePath) == false):
+		if(file_exists($imagePath) == false){
 			return 'image not found';
-		endif;
-	endif;
-
-	if(isset($opts['w'])): $w = $opts['w']; endif;
-	if(isset($opts['h'])): $h = $opts['h']; endif;
-
+		}
+	}
+	if(isset($opts['w'])){ $w = $opts['w']; };
+	if(isset($opts['h'])){ $h = $opts['h']; };
 	$filename = md5_file($imagePath);
-
 	// If the user has requested an explicit output-filename, do not use the cache directory.
-	if(false !== $opts['output-filename']) :
+	if($opts['output-filename'] !== false){
 		$newPath = $opts['output-filename'];
-	else:
-        if(!empty($w) and !empty($h)):
-            $newPath = $cacheFolder.$filename.'_w'.$w.'_h'.$h.(isset($opts['crop']) && $opts['crop'] == true ? "_cp" : "").(isset($opts['scale']) && $opts['scale'] == true ? "_sc" : "").'.'.$ext;
-        elseif(!empty($w)):
+	}else{
+        if(!empty($w) and !empty($h)){
+            $newPath = $cacheFolder.$filename.'_w'.$w.'_h'.$h.($opts['crop'] == true ? "_cp" : "").($opts['scale'] == true ? "_sc" : "").'.'.$ext;
+        }else if(!empty($w)){
             $newPath = $cacheFolder.$filename.'_w'.$w.'.'.$ext;	
-        elseif(!empty($h)):
+        }else if(!empty($h)){
             $newPath = $cacheFolder.$filename.'_h'.$h.'.'.$ext;
-        else:
+        }else{
             return false;
-        endif;
-	endif;
-
+        }
+	}
 	$create = true;
-
-    if(file_exists($newPath) == true):
+    if(file_exists($newPath) == true){
         $create = false;
         $origFileTime = date("YmdHis",filemtime($imagePath));
         $newFileTime = date("YmdHis",filemtime($newPath));
-        if($newFileTime < $origFileTime): # Not using $opts['expire-time'] ??
+        if($newFileTime < $origFileTime){					# Not using $opts['expire-time'] ??
             $create = true;
-        endif;
-    endif;
-
-	if($create == true):
-		if(!empty($w) and !empty($h)):
-
+        }
+    }
+	if($create == true){
+		if(!empty($w) and !empty($h)){
 			list($width,$height) = getimagesize($imagePath);
 			$resize = $w;
-		
-			if($width > $height):
+			if($width > $height){
 				$resize = $w;
-				if(true === $opts['crop']):
+				if($opts['crop'] == true){
 					$resize = "x".$h;				
-				endif;
-			else:
+				}
+			}else{
 				$resize = "x".$h;
-				if(true === $opts['crop']):
+				if($opts['crop'] == true){
 					$resize = $w;
-				endif;
-			endif;
-
-			if(true === $opts['scale']):
+				}
+			}
+			if($opts['scale'] == true){
 				$cmd = $path_to_convert ." ". escapeshellarg($imagePath) ." -resize ". escapeshellarg($resize) . 
 				" -quality ". escapeshellarg($opts['quality']) . " " . escapeshellarg($newPath);
-			else:
+			}else{
 				$cmd = $path_to_convert." ". escapeshellarg($imagePath) ." -resize ". escapeshellarg($resize) . 
 				" -size ". escapeshellarg($w ."x". $h) . 
 				" xc:". escapeshellarg($opts['canvas-color']) .
 				" +swap -gravity center -composite -quality ". escapeshellarg($opts['quality'])." ".escapeshellarg($newPath);
-			endif;
-						
-		else:
+			}
+		}else{
 			$cmd = $path_to_convert." " . escapeshellarg($imagePath) . 
 			" -thumbnail ". (!empty($h) ? 'x':'') . $w ."". 
-			(isset($opts['maxOnly']) && $opts['maxOnly'] == true ? "\>" : "") . 
+			($opts['maxOnly'] == true ? "\>" : "") . 
 			" -quality ". escapeshellarg($opts['quality']) ." ". escapeshellarg($newPath);
-		endif;
-
+		}
 		$c = exec($cmd, $output, $return_code);
         if($return_code != 0) {
             error_log("Tried to execute : $cmd, return code: $return_code, output: " . print_r($output, true));
             return false;
 		}
-	endif;
+	}
 
-	# return cache file path
-	return str_replace($_SERVER['DOCUMENT_ROOT'],'',$newPath);
-	
+	// return cache file path
+	return str_replace($_SERVER['DOCUMENT_ROOT'],'',$newPath);	
 }
